79a2a677916f21263f6ba8f70747894ecc10c8f2
Aizat Azhar <39623254+aizatazhar@users.noreply.github.com>
2023-04-12 00:21:28 +0800

    Add search.py template

---
+++ search.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import sys
+import getopt
+
+def usage():
+    print(
+        "usage: "
+        + sys.argv[0]
+        + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
+    )
+
+def run_search(dict_file, postings_file, queries_file, results_file):
+    """
+    using the given dictionary file and postings file,
+    perform searching on the given queries file and output the results to a file
+    """
+    print("running search on the queries...")
+
+try:
+    opts, args = getopt.getopt(sys.argv[1:], "d:p:q:o:")
+except getopt.GetoptError:
+    usage()
+    sys.exit(2)
+
+for o, a in opts:
+    if o == "-d":
+        dictionary_file = a
+    elif o == "-p":
+        postings_file = a
+    elif o == "-q":
+        file_of_queries = a
+    elif o == "-o":
+        file_of_output = a
+    else:
+        assert False, "unhandled option"
+
+run_search(dictionary_file, postings_file, file_of_queries, file_of_output)
