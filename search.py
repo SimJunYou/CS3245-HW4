c25f9a50cdba27a1c30db6fd40ad9e6a0c0c8b6e
Aizat Azhar <39623254+aizatazhar@users.noreply.github.com>
2023-04-16 12:46:16 +0800

    Update query expansion to use custom thesaurus

--- search.py
+++ search.py
@@ -2,6 +2,8 @@
 import sys
 import getopt
 
+from typing import *
+
 def usage():
     print(
         "usage: "
@@ -9,29 +11,40 @@ def usage():
         + " -d dictionary-file -p postings-file -q file-of-queries -o output-file-of-results"
     )
 
-def run_search(dict_file, postings_file, queries_file, results_file):
+def run_search(dict_file: str, postings_file: str, queries_file: str, results_file: str):
     """
     using the given dictionary file and postings file,
     perform searching on the given queries file and output the results to a file
     """
     print("running search on the queries...")
 
-try:
-    opts, args = getopt.getopt(sys.argv[1:], "d:p:q:o:")
-except getopt.GetoptError:
-    usage()
-    sys.exit(2)
+def expand_query(tokens: List[str]):
+    with open("thesaurus.txt", "r") as f:
+        thesaurus = eval(f.read())
+    result = []
+    for token in tokens:
+        result.append(token)
+        result += thesaurus.get(token, [])
+    return result
 
-for o, a in opts:
-    if o == "-d":
-        dictionary_file = a
-    elif o == "-p":
-        postings_file = a
-    elif o == "-q":
-        file_of_queries = a
-    elif o == "-o":
-        file_of_output = a
-    else:
-        assert False, "unhandled option"
+if __name__ == '__main__':
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "d:p:q:o:")
+    except getopt.GetoptError:
+        usage()
+        sys.exit(2)
 
-run_search(dictionary_file, postings_file, file_of_queries, file_of_output)
+    for o, a in opts:
+        if o == "-d":
+            dictionary_file = a
+        elif o == "-p":
+            postings_file = a
+        elif o == "-q":
+            file_of_queries = a
+        elif o == "-o":
+            file_of_output = a
+        else:
+            assert False, "unhandled option"
+
+    # run_search(dictionary_file, postings_file, file_of_queries, file_of_output)
+    print(expand_query(["plaintiff"]))
